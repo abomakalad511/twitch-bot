6da4dad14570a86ca6d26a2c061904deb0771d88
abomakalad511 <abomakalad511@gmail.com>
2026-04-19 02:40:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,11 +29,62 @@ client.on('message', (channel, tags, message, self) => {
 
   if (msg === 'هل انت انسان') {
   client.say(channel, 'انا مملوك لبونايف 🤖');
-}
+  }
 
   setInterval(() => {
   client.say(channel, '💜 لا تنسى تتابع القناة!');
 }, 300000);
 
-  
+
+  const commands = {
+  '!هلا': (channel, user) => {
+    return `هلا والله ${user} 👋`;
+  },
+
+  '!فولو': () => {
+    return '💜 لا تنسى تتابع القناة!';
+  },
+
+  '!متابعين': () => {
+    return '📊 عدد المتابعين قريب نوصل هدفنا 🔥';
+  },
+
+  '!دسكورد': () => {
+    return '📢 رابط الدسكورد: حطه هنا';
+  },
+
+  '!قوانين': () => {
+    return '🚫 ممنوع السب - 🚫 سبام - استمتعوا 🤍';
+  },
+
+  '!انستا': () => {
+    return '📸 حساب الانستقرام: https://www.instagram.com/';
+  },
+
+  '!تيك': () => {
+    return '🎵 حساب التيك توك: https://www.tiktok.com/@zp1dr';
+  },
+
+  '!يوتيوب': () => {
+    return '▶️ قناتي في اليوتيوب: حطه هنا';
+  },
+
+  '!اب': () => {
+    return '🔥 لا تنسى اللايك يا وحش!';
+  },
+
+  '!بث': () => {
+    return '🔴 البث شغال لا تفوتك!';
+  }
+};
+
+// الردود
+if (commands[msg]) {
+  client.say(channel, commands[msg](channel, tags.username));
+}
+
+// سؤال البوت
+if (msg.includes('هل انت انسان')) {
+  client.say(channel, 'انا مملوك لبونايف 🤖');
+}
 });
